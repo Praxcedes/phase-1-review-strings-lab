f1ce031a216e6762d589a0ce18dd4a80e7292bb7
Praxcedes <praxcedeskabeya@gmail.com>
2025-06-12 07:52:42 -0700

    phase-1-review-strings-lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+//Define the current user
+const currentUser = 'Grace Hopper';
+//Creat a welcome message using the currentUser
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//Creat an excited welcome message in uppercase
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+//Creat a short greeting using the first initial
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
